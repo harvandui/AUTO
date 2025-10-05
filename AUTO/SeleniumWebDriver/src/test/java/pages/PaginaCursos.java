c347d40cc02e052a6740596249cd9de3650a5fce
harvandui <arbetle@hotmail.com>
2025-10-04 22:37:25 -0500

    Delete AUTO/SeleniumWebDriver directory

--- AUTO/SeleniumWebDriver/src/test/java/pages/PaginaCursos.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-public class PaginaCursos extends BasePage {
-    public String introTesting = "//a[normalize-space()='Introducción al Testing de Software' and @href]";
-
-    public void clickIntroTesting() {
-        clickElement(introTesting);
-    }
-}
--- AUTO/SeleniumWebDriver/src/test/java/pages/PaginaIntroTesting.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-public class PaginaIntroTesting extends BasePage {
-    public String comprar = "//a[@class='sc-dJkDXt fodGEh']";
-
-    public void clickComprar() {
-        clickElement(comprar);
-    }
-}
